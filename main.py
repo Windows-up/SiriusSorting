dbf25c97e7438b2ba0910eac3d7b3a966e1a5b98
jebpi <jebpip2008@gmail>
2022-02-10 10:00:53 +0300

    2 errors check 1.0

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
-from lib import *
+from lib import damerau_levenshtein_distance
+from twoWordTesting import check2
 
 dictionary = open("dict.txt", "r").read().split("\n")
 queries = open("queries.txt", "r").read().split("\n")
 
 
+
 def main():
 
     for i in range(0, len(queries)):
+        file = open("out.txt", "w")
 
         # Find original word
         word = queries[i]
@@ -14,24 +17,35 @@ def main():
 
         minimum = 999
         for original_word in dictionary:
+            if minimum:
+                dist = damerau_levenshtein_distance(word, original_word)
 
-            dist = damerau_levenshtein_distance(word, original_word)
-            if dist < minimum:
-                original = original_word
-                minimum = dist
 
-        check = check2(word, original)
+                if dist < minimum:
+                    original = original_word
+                    minimum = dist
+
 
         if minimum >= 3:
             minimum = "3+"
 
-        elif minimum == 0 and check:
-            print(check)
+        if minimum == 0 :
+            print(f"{word} 0 {original}")
+            file.write(f"{word} 0 {original} \n")
+
+        elif minimum == 2:
+            print(check2(word,original))
+            file.write(check2(word,original) + "\n")
 
         else:
-            result = f"{word} {minimum} {original}"
+            result = f"{word} {minimum} {original} \n"
             print(result)
+            file.write(result)
+
+        print(i)
+        file.close()
 
 
 if __name__ == "__main__":
     main()
+
--- twoWordTesting.py
+++ twoWordTesting.py
@@ -1,58 +1,57 @@
 import difflib
 
-
-
-a = "прээет"
-b = "привет"
-
-cases = [(a, b)]
-
-position = 0
-
-for a, b in cases:
+def check2(query, dict_word):
+    position = 0
     operations = []
-    print('{} => {} \n'.format(a, b))
-    for i, s in enumerate(difflib.ndiff(a, b)):
-        res = ""
+    # print('{} => {} \n'.format(a, b))
+    for i, s in enumerate(difflib.ndiff(query, dict_word)):
+        # res = ""
         if s[0] == ' ':
             continue
         elif s[0] == '-':
-            res = f'Delete "{s[-1]}" from position {i}'
-            operations.append(["-",s[-1],i])
+            # res = f'Delete "{s[-1]}" from position {i}'
+            operations.append(["-", s[-1], i])
             position += 1
 
         elif s[0] == '+':
-            res = f'Add "{s[-1]}" to position {i}'
+            # res = f'Add "{s[-1]}" to position {i}'
             operations.append(["+", s[-1], i])
 
+        # print(res)
+
+    # print(*operations)
+    last_operation = [0, 0, 0]
+
+    massive = []
+    for i in query:
+        massive.append(i)
+    # print(massive)
+
+    itr = []
+    itog = ""
+    for operation in operations:
+        if last_operation[0] == "-" and last_operation[2] == operation[2] - 1:
+            itr.append(['/', operation[1], operation[2]])
+            # massive[operation[2]-position] = operation[1]
+            # print(massive)
+        last_operation = operation
+        # print(itr[-1:])
+        itog = ['/', operation[1], operation[2]]
+        # print(f"itog = {itog}")
+
+    # print(itog)
+    massive[itog[2] - position] = itog[1]
+    # print(*a)
+    # print(*massive)
+    # print(*b)
+    temp_string = ""
+    for char in massive:
+        temp_string += char
+    return f"{query} 2 {temp_string} {dict_word}"
 
 
-        #print(res)
+if __name__ == "__main__":
+    a = "привет"
+    b = "привет"
 
-#print(*operations)
-last_operation = [0,0,0]
-
-massive = []
-for i in a:
-    massive.append(i)
-#print(massive)
-
-itr = []
-itog = ""
-for operation in operations:
-    if last_operation[0] == "-" and last_operation[2] == operation[2] - 1 :
-        itr.append(['/',operation[1],operation[2]])
-        # massive[operation[2]-position] = operation[1]
-        # print(massive)
-    last_operation = operation
-    #print(itr[-1:])
-    itog = ['/',operation[1],operation[2]]
-    #print(f"itog = {itog}")
-
-#print(itog)
-massive[itog[2]-position] = itog[1]
-print(*a)
-print(*massive)
-print(*b)
-
-
+    print(check2(a, b))
